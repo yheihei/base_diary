5be2016edef418d9dbad180ab717c478d6f67b3c
Yohei Kokubo <yhei.register@gmail.com>
2021-06-13 09:09:37 +0900

    フォームのcssを整える - UpdateViewを使ってみる #3

--- diary/forms.py
+++ diary/forms.py
@@ -5,3 +5,9 @@ class PostForm(forms.ModelForm):
   class Meta:
     model = Post
     fields = ('user', 'title', 'body')
+  
+  def __init__(self, *args, **kwargs):
+    super(PostForm, self).__init__(*args, **kwargs)
+    self.fields['user'].widget.attrs.update({'class' : 'form-control', 'readonly': 'readonly'})
+    self.fields['title'].widget.attrs.update({'class' : 'form-control'})
+    self.fields['body'].widget.attrs.update({'class' : 'form-control'})
