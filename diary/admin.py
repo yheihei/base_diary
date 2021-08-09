9a740b9a2d8fea727b4bee181a653c3f1cf4c424
Yohei Kokubo <yhei.register@gmail.com>
2021-08-09 14:53:20 +0900

    adminにStarを追加 - UI操作の自動テスト #6

--- diary/admin.py
+++ diary/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Post, Category
+from .models import User, Post, Category, Star
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Star)
