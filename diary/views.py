10cff743a20177e460300c6253da3bd0c769e45c
Yohei Kokubo <yhei.register@gmail.com>
2021-09-03 07:48:22 +0900

    ページネーション追加 - ListViewを極める #13

--- diary/views.py
+++ diary/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from diary.models import Post
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.db.models import Prefetch
 
 # Create your views here.
@@ -29,9 +29,20 @@ class PostListView(ListView):
     'categories',  # あらかじめcategoryテーブルをjoinしておく
   )
 
-  # コンテキストを追加する
-  # https://docs.djangoproject.com/ja/3.2/topics/class-based-views/generic-display/#adding-extra-context
+  # ページネーション
+  paginate_by = 2
+  page_kwarg = 'page'  # 未指定でも良い。デフォルトは'page'
+
+  pagenate_root_url = reverse_lazy('diary:index')
+
   def get_context_data(self, **kwargs):
+    '''
+    コンテキストを追加する
+
+    Notes
+    -----
+    https://docs.djangoproject.com/ja/3.2/topics/class-based-views/generic-display/#adding-extra-context
+    '''
     context = super().get_context_data(**kwargs)
     # ログインしていたら、postに編集用のURLを付与する
     if self.request.user.is_authenticated:
@@ -39,14 +50,39 @@ class PostListView(ListView):
       for post in posts:
         post.edit_url = reverse(f'admin:{post._meta.app_label}_{post._meta.model_name}_change', args=[post.id] )
       context['posts'] = posts
+    
+    # 検索条件やper_pageを含んだページネーション用URL
+    self.pagenate_root_url += '?'
+    query_dict = self.request.GET.copy()
+    query_dict.pop(self.page_kwarg)  # ページ番号だけは除く
+    for key, value in query_dict.items():
+      self.pagenate_root_url += f'&{key}={value}'
+    context['pagenate_root_url'] = self.pagenate_root_url
     return context
 
-  # 動的なフィルタリング
-  # https://docs.djangoproject.com/ja/3.2/topics/class-based-views/generic-display/#dynamic-filtering
   def get_queryset(self, **kwargs):
+    '''
+    動的なフィルタリング
+
+    Notes
+    -----
+    https://docs.djangoproject.com/ja/3.2/topics/class-based-views/generic-display/#dynamic-filtering
+    '''
     queryset = super().get_queryset(**kwargs)
     if self.request.GET.get('category'):
       queryset = queryset.filter(
         categories__slug=self.request.GET.get('category')
       )
     return queryset
+
+  def get_paginate_by(self, queryset):
+    '''
+    per_pageをクエリによって動的に変える
+
+    Notes
+    -----
+    https://github.com/django/django/blob/stable/3.2.x/django/views/generic/list.py
+    '''
+    if self.request.GET.get('per_page'):
+      self.paginate_by = int(self.request.GET.get('per_page'))
+    return super().get_paginate_by(queryset)
