114a8f20d2e399c2b820c79963b1690890e09717
Yohei Kokubo <yhei.register@gmail.com>
2021-09-14 13:11:46 +0900

    contextを編集する - DetailViewの使い方をマスターしよう #14

--- diary/views.py
+++ diary/views.py
@@ -3,6 +3,7 @@ from django.views.generic.detail import DetailView
 
 # Create your views here.
 from .models import Post
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
@@ -13,6 +14,8 @@ def index(request):
 
 class PostDetailView(DetailView):
   model = Post
+  # テンプレートの名前を指定する
+  template_name = 'diary/post_detail.html'  # 未指定の場合 アプリ名/post_detail.html
   # 取得するクエリを指定
   queryset = Post.objects.select_related(
     'user',  # あらかじめuserテーブルをjoinしておく
@@ -21,4 +24,16 @@ class PostDetailView(DetailView):
   )
   # objectの名前を変更する
   context_object_name = 'post'  # 指定しない場合 object という名前でcontextに渡される
+
+  def get_context_data(self, **kwargs):
+    '''
+    contextを編集する
+    '''
+    # contextの取得
+    context = super().get_context_data(**kwargs)
+    # 編集ページのURLを追加
+    if self.request.user.is_authenticated and context[self.context_object_name]:
+      post = context[self.context_object_name]
+      post.edit_url = reverse(f'admin:{post._meta.app_label}_{post._meta.model_name}_change', args=[post.id] )
+    return context
 
