85e195f52860c90744c6feb160612bede4bb94a4
Yohei Kokubo <yhei.register@gmail.com>
2021-09-20 21:38:00 +0900

    CreateViewを使ってページ表示まで - CreateViewの使い方をマスターしよう #15

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "diary"
 urlpatterns = [
   path("", views.index, name="index"),
+  path('add/', views.PostCreateView.as_view(), name='post-add'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from .models import Post
+from django.views.generic.edit import CreateView
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {
     'posts': Post.objects.all(),
   })
+
+
+class PostCreateView(CreateView):
+  model = Post
+  fields = ['title', 'body']
+
+
+# ログイン必須にする
+
+# ログイン時、そのユーザーの投稿として保存する
+
+# フォームにCSSをあてる
+
+# 成功した際の遷移先を決める
