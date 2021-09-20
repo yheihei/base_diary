4c55a38485bf250a55854e735803e153a27552c9
Yohei Kokubo <yhei.register@gmail.com>
2021-09-20 22:20:30 +0900

    保存時に特殊な処理をする - CreateViewの使い方をマスターしよう #15

--- diary/views.py
+++ diary/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -16,10 +18,25 @@ class PostCreateView(LoginRequiredMixin, CreateView):
   login_url = '/admin/login/'
   model = Post
   fields = ['title', 'body']
+  # 保存したら一覧に遷移するようにする(暫定)
+  success_url = reverse_lazy('diary:index')
 
+  def form_valid(self, form):
+    '''
+    投稿の保存処理
+    '''
+    # ログイン中のユーザーを投稿者として保存する
+    self.object = form.save(commit=False)
+    self.object.user = self.request.user
+    self.object.save()
+    return HttpResponseRedirect(super().get_success_url())
 
-# ログイン時、そのユーザーの投稿として保存する
+# カテゴリーを選べるようにする
+
+# 特殊なバリデーションを付与する
 
 # フォームにCSSをあてる
 
-# 成功した際の遷移先を決める
+# エラー時のフォームにCSSをあてる
+
+# 成功したら詳細画面に遷移するようにする
