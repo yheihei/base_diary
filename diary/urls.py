b0218ccfa88f2d65627c734beab2c68d9fbbcbfa
Yohei Kokubo <yhei.register@gmail.com>
2021-10-03 10:40:14 +0900

    本実装  - DeleteViewの使い方をマスターしよう #17

--- diary/urls.py
+++ diary/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "diary"
 urlpatterns = [
   path("", views.index, name="index"),
+  path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
--- diary/views.py
+++ diary/views.py
@@ -8,3 +8,17 @@ def index(request):
   return render(request, 'index.html', {
     'posts': Post.objects.all(),
   })
+
+
+from django.urls import reverse_lazy
+from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+  # ログインURLも指定する
+  login_url = reverse_lazy('admin:login')
+  model = Post
+  success_url = reverse_lazy('diary:index')
+
+  # template_name = 'post_confirm_delete.html'  # デフォルトはアプリ名/モデル名_confirm_delete.html
